df0c4432039bfc65aeac3ec6c56f24d2d8df1c2c
Gaborerdei <>
2023-08-21 19:49:08 +0000

    Set up structure of game functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,21 @@
-console.log("connected");
+function runGame() {
+
+}
+function checkAnswer() {
+
+}
+function calculateCorrectAnswer() {
+
+}
+
+function incremenScore() {
+
+}
+
+function incrementWrongAnswer() {
+
+}
+
+function displayQuestion() {
+
+}
