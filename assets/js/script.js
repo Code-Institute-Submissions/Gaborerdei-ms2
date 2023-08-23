cd63ef39ffcb3a58d4b8c304cbbab59c3960f176
Gaborerdei <>
2023-08-23 18:59:49 +0000

    Testing display question (not working)

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,43 @@
 //Wait for the DOM to finish loading before run the game
 //Get the button elements and add eventlistehers to them
+
 document.addEventListener("DOMContentLoaded", function () {
-    let buttons = this.getElementsByTagName("button");
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function () {
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You clicked Submit!");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                runGame(gameType);
+            }
+        });
+    }
+
+    runGame("addition");
+
 });
 
-//Creates two random numbers between 0 and 5
-function runGame() {
-    let num1 = Math.floor(Math.random) * 5;
-    let num2 = Math.floor(Math.random) * 5;
+
+
+
+/**The main game "loop" called when the script is first loaded and
+ * after user's answer has been processed
+ */
+function runGame(gameType) {
+    //Creates two random numbers between 0 and 5
+    let num1 = Math.floor(Math.random) * 6;
+    let num2 = Math.floor(Math.random) * 6;
+
+    if (gameType === "addition") {
+        displayQuestion(num1, num2);
+    } else {
+        alert(`Unknown game type:`);
+        throw `Unknown game type: Aborting!`;
+    }
 }
+
 function checkAnswer() {
 
 }
@@ -24,6 +53,9 @@ function incrementWrongAnswer() {
 
 }
 
-function displayQuestion() {
+function displayQuestion(operand1, operand2) {
 
+    document.getElementById('operand1').textcontent = operand1;
+    document.getElementById('operand2').textcontent = operand2;
+    document.getElementById('operator').textcontent = "+";
 }
