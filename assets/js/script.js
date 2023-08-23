739955ec3bd5992b624c554d673284bf86c09fe2
Gaborerdei <>
2023-08-23 19:28:01 +0000

    "displayquestion" funcion added and working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 //Wait for the DOM to finish loading before run the game
 //Get the button elements and add eventlistehers to them
+console.log("aaa");
 
 document.addEventListener("DOMContentLoaded", function () {
     let buttons = document.getElementsByTagName("button");
@@ -27,14 +28,14 @@ document.addEventListener("DOMContentLoaded", function () {
  */
 function runGame(gameType) {
     //Creates two random numbers between 0 and 5
-    let num1 = Math.floor(Math.random) * 6;
-    let num2 = Math.floor(Math.random) * 6;
+    let num1 = Math.floor(Math.random() * 6);
+    let num2 = Math.floor(Math.random() * 6);
 
     if (gameType === "addition") {
         displayQuestion(num1, num2);
     } else {
         alert(`Unknown game type:`);
-        throw `Unknown game type: Aborting!`;
+
     }
 }
 
@@ -55,7 +56,8 @@ function incrementWrongAnswer() {
 
 function displayQuestion(operand1, operand2) {
 
-    document.getElementById('operand1').textcontent = operand1;
-    document.getElementById('operand2').textcontent = operand2;
-    document.getElementById('operator').textcontent = "+";
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "+";
+
 }
