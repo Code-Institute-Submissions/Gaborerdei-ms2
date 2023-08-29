fecab54cc7cde778aa03696d663b4e0964767957
Gaborerdei <>
2023-08-29 23:03:34 +0000

    Display game area and user-input area deleted. works with css

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,29 +2,27 @@
 
 document.addEventListener("DOMContentLoaded", function () {
 
-    //display user input block
-    document.getElementById("user-input").style.display = "block";
-
-    //hide game input block
-    document.getElementById("game").style.display = "none";
 });
 
-
 let gname = "Guest";
-document.getElementById("submit-btn").addEventListener("click", function (e) {
 
-    //hide user input block
-    document.getElementById("user-input").style.display = "none";
+document.getElementById("submit-btn").addEventListener("click", function (e) {
 
     //show game block
     document.getElementById("game").style.display = "block";
 
+    //hide user input block
+    document.getElementById("user-input").style.display = "none";
+
     //set correct name
     let inputElement = document.getElementById("user");
-    if (inputElement.value.lenght > 0)
+    if (inputElement.value.length > 0)
         gname = document.getElementById("user").value;
+    debugger;
 
     //update game text
-    document.getElementById("game-text").innerText = gname + ", what is the correct answer?";
+    document.getElementById("game-text").innerText = gname + ",what is the correct answer?";
 });
+
+
 
