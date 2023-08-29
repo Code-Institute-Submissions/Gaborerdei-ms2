7eaf1a90941f1412f2d4e530624b375d6e79bbad
Gaborerdei <>
2023-08-29 20:12:13 +0000

    Add event listener to Submit button.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,63 +1,10 @@
 //Wait for the DOM to finish loading before run the game
-//Get the button elements and add eventlistehers to them
-console.log("aaa");
 
 document.addEventListener("DOMContentLoaded", function () {
-    let buttons = document.getElementsByTagName("button");
-
-    for (let button of buttons) {
-        button.addEventListener("click", function () {
-            if (this.getAttribute("data-type") === "submit") {
-                alert("You clicked Submit!");
-            } else {
-                let gameType = this.getAttribute("data-type");
-                runGame(gameType);
-            }
-        });
-    }
-
-    runGame("addition");
 
 });
 
+document.getElementById("submit-btn").addEventListener("click", function (e) {
 
 
-
-/**The main game "loop" called when the script is first loaded and
- * after user's answer has been processed
- */
-function runGame(gameType) {
-    //Creates two random numbers between 0 and 5
-    let num1 = Math.floor(Math.random() * 6);
-    let num2 = Math.floor(Math.random() * 6);
-
-    if (gameType === "addition") {
-        displayQuestion(num1, num2);
-    } else {
-        alert(`Unknown game type:`);
-
-    }
-}
-
-function checkAnswer() {
-
-}
-function calculateCorrectAnswer() {
-
-}
-
-function incrementScore() {
-
-}
-
-function incrementWrongAnswer() {
-
-}
-
-function displayQuestion(operand1, operand2) {
-
-    document.getElementById('operand1').textContent = operand1;
-    document.getElementById('operand2').textContent = operand2;
-    document.getElementById('operator').textContent = "+";
-
-}
+};
