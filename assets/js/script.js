d6fc8d9befd3794a9d572097e6ae2fc0b4409fdb
Gaborerdei <>
2023-08-29 21:06:23 +0000

    After click Submit button hide user input area and display Game area

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,20 @@
 
 document.addEventListener("DOMContentLoaded", function () {
 
+    //display user input block
     document.getElementById("user-input").style.display = "block";
+
+    //hide game input block
     document.getElementById("game").style.display = "none";
+});
+
+
+document.getElementById("submit-btn").addEventListener("click", function (e) {
+
+    //hide user input block
+    document.getElementById("user-input").style.display = "none";
+
+    //show game block
+    document.getElementById("game").style.display = "block";
 });
 
