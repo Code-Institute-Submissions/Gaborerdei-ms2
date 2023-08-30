bfb2049aae64b3d68a0c348b27aeb400620ed739
Gaborerdei <>
2023-08-30 20:53:16 +0000

    Start button event listener added to start the timer.

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,29 +24,29 @@ document.getElementById("submit-btn").addEventListener("click", function (e) {
 });
 
 
+document.getElementById("start-btn").addEventListener("click", function (e) {
+    //Timer 30 second
 
-//Timer 30 second
+    // Set the target countdown time in seconds
+    const targetTime = 32; // 0.5 minutes
 
-// Set the target countdown time in seconds
-const targetTime = 32; // 0.5 minutes
+    // Calculate the end time
+    const endTime = new Date().getTime() + (targetTime * 1000);
 
-// Calculate the end time
-const endTime = new Date().getTime() + (targetTime * 1000);
+    // Update the timer every second
+    timer = setInterval(updateTimer, 1000);
 
-// Update the timer every second
-timer = setInterval(updateTimer, 1000);
+    function updateTimer() {
+        const currentTime = new Date().getTime();
+        const timeLeft = Math.max(0, endTime - currentTime);
 
-function updateTimer() {
-    const currentTime = new Date().getTime();
-    const timeLeft = Math.max(0, endTime - currentTime);
+        const seconds = Math.floor(timeLeft / 1000);
 
-    const seconds = Math.floor(timeLeft / 1000);
+        document.getElementById('timer').textContent = seconds;
 
-    document.getElementById('timer').textContent = seconds;
-
-    if (timeLeft === 0) {
-        clearInterval(timer);
-        document.getElementById('timer').textContent = '   Time\'s up!';
+        if (timeLeft === 0) {
+            clearInterval(timer);
+            document.getElementById('timer').textContent = '   Time\'s up!';
+        }
     }
-}
-
+});
